9a12e3c34747d4622f8da7ed89df31d45d073951
sigolyshkov <sigolyshkov@yandex.ru>
2022-12-03 21:18:25 +0400

    modal

--- js/common.js
+++ js/common.js
@@ -5,4 +5,19 @@ $(document).ready(function(){
         slidesToScroll: 1,
         dots: false,
     });
+
+    $('a[href^="#"]').on('click', () => {
+        let anchor = $(this).attr('href');  
+        $('html, body').animate({           
+            scrollTop:  $(anchor).offset().top  
+        }, 600);                            
+    });
+
+    $('.onModal').on('click', () => {
+        $('.modal').addClass('open');
+    })
+
+    $('.modal__close').on('click', () => {
+        $('.modal').removeClass('open');
+    })
 });
